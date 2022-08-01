f047146c0a07861bbef96da3e5da710c97925b31
maksi <maksimbaltak@yandex.ru>
2022-08-01 08:52:32 +0200

    сделана вёрстка чата.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AuthPageComponent} from "./pages/auth-page/auth-page.component";
+import {ChatComponent} from "./pages/chat/chat.component";
 
 const routes: Routes = [
-  {path: '', component: AuthPageComponent}
+  {path: '', component: AuthPageComponent},
+  {path: 'chat', component: ChatComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthPageComponent } from './pages/auth-page/auth-page.component';
-
+import { ChatComponent } from './pages/chat/chat.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { UserComponent } from './components/user/user.component';
+import {AvatarComponent} from "./components/avatar/avatar.component";
+import { ChatContainerComponent } from './components/chat-container/chat-container.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MessageComponent } from './components/message/message.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AuthPageComponent
+    AuthPageComponent,
+    ChatComponent,
+    MenuComponent,
+    UserComponent,
+    AvatarComponent,
+    ChatContainerComponent,
+    HeaderComponent,
+    MessageComponent,
   ],
   imports: [
     BrowserModule,
